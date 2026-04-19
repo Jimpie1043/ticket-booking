e4d4bb967c74665580dc15cc431d3802e1f2575c
FX <fx.leclerc05@gmail.com>
2026-04-19 01:54:50 -0400

    db drop all

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 from app import create_app
-from dotenv import load_dotenv
+from app.extensions import db
+from seed import run_seed
 import os
 
-load_dotenv()
-
 app = create_app()
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    run_seed()
 
 if __name__ == "__main__":
     debug = os.getenv("FLASK_ENV") != "production"
