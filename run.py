9e7a1db2da1814c56798a0f2a7935b53018c3e32
FX <fx.leclerc05@gmail.com>
2026-04-19 02:14:10 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 from app import create_app
-from dotenv import load_dotenv
+from flask_migrate import upgrade
+from seed import run_seed
 import os
 
-load_dotenv()
-
 app = create_app()
+
+with app.app_context():
+    upgrade()
+    run_seed(app)
 
 if __name__ == "__main__":
     debug = os.getenv("FLASK_ENV") != "production"
