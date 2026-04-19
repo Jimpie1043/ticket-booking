be7e541801cda0e6db909f9b1bd36ed0dc00bd9e
FX <fx.leclerc05@gmail.com>
2026-04-18 21:33:03 -0400

    Préparé pour production

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "devkey")
-    
+
     SQLALCHEMY_DATABASE_URI = (
         "sqlite:///" + os.path.join(
             os.path.abspath(os.path.dirname(__file__)),
@@ -11,11 +14,15 @@ class Config:
             "site.db"
         )
     )
-    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
+    # Détection d'environnement (prod/développement)
+    ENV = os.getenv("FLASK_ENV", "development")
+    IS_PROD = ENV == "production"
+
+    # Sécurité
     SESSION_COOKIE_SAMESITE = "Lax"
     SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SECURE = False  # Change to true in production
+    SESSION_COOKIE_SECURE = IS_PROD
 
-    FORCE_HTTPS = False  # Change to true in production
+    FORCE_HTTPS = IS_PROD
--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from app import create_app
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    debug = os.getenv("FLASK_ENV") != "production"
+    app.run(debug=debug) # Pour développement seulement
